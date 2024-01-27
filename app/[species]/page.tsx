09e27bf1971b9b95145bc8f6785852f97ae44572
Connor Sullivan <connor.sullivan@kraken.tech>
2024-01-27 11:24:46 -0500

    Fetch bird pages from filesystem
    Not performant since we access the filesystem for every page, but
    will suffice until cloud object storage is configured.

--- app/[species]/page.tsx
+++ app/[species]/page.tsx
@@ -1,30 +1,43 @@
-import type { PageProps } from "@/.next/types/app/page";
-
-export const dynamicParams = false;
+import Image from "next/image";
+import fs from "fs";
 
 type Bird = {
   species: string;
+  link: string;
 };
 
-export async function generateStaticParams(): Promise<Bird[]> {
-  // TODO:   Loop through bird pictures in files system
+async function getBirds(): Promise<Bird[]> {
+  const BIRD_PIC_DIR = "/Users/connor.sullivan/practice/birddave/public/pics";
+  const birds: Bird[] = [];
 
-  return [
-    { species: "robin" },
-    { species: "jay" },
-    { species: "magpie" },
-    { species: "crow" },
-  ];
+  const files = await fs.promises.readdir(BIRD_PIC_DIR);
+
+  for (const file of files) {
+    const [species, ..._] = file.split(".");
+    birds.push({ species: species, link: file });
+  }
+
+  return birds;
+}
+
+export async function generateStaticParams(): Promise<Bird[]> {
+  return await getBirds();
 }
 
 // Multiple versions of this page will be statically generated
 // using the `params` returned by `generateStaticParams`
-export default function Page({ params }: PageProps) {
-  const { species } = params;
+export default async function Page({ params }) {
+  const birds = await getBirds();
+  const bird = birds.find((bird) => bird.species === params.species);
+
+  if (!bird) {
+    return <div>404</div>;
+  }
 
   return (
     <div>
-      <h1>{species}</h1>
+      <h1>Species: {bird.species}</h1>
+      <Image src={`/pics/${bird.link}`} alt="" height="100" width="150" />
     </div>
   );
 }
