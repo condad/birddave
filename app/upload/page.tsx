8cdaf06c3ff487bc7855523ffd1916db099e383e
Connor Sullivan <connor.sullivan@kraken.tech>
2024-02-16 00:26:33 -0500

    Specify species on picture upload

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,27 +1,70 @@
-import { Inter } from "next/font/google";
 import { Bucket } from "sst/node/bucket";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
-import UploadForm from "./upload";
-
-const inter = Inter({ subsets: ["latin"] });
-
-async function getPresignedUrl(): Promise<string> {
-  const client = new S3Client();
-  const command = new PutObjectCommand({
-    Bucket: Bucket.public.bucketName,
-    Key: "another-1.jpg",
-  });
-
-  return await getSignedUrl(client, command, { expiresIn: 3600 });
-}
 
 export default async function Page() {
-  const url = await getPresignedUrl();
+  async function uploadPicture(formData: FormData) {
+    "use server";
+
+    const file = formData.get("file") as File;
+    const species = formData.get("species") as string;
+    const number = formData.get("number") as string;
+
+    const fileArrayBuffer = await file.arrayBuffer();
+    const fileBuffer = Buffer.from(fileArrayBuffer);
+
+    const client = new S3Client();
+    const command = new PutObjectCommand({
+      Body: fileBuffer,
+      Bucket: Bucket.public.bucketName,
+      Key: `${species}-${number}.jpg`,
+      ContentType: file.type,
+    });
+
+    const response = await client.send(command);
+
+    console.log(response);
+  }
 
   return (
-    <main>
-      <UploadForm url={url} />
+    <main className="w-full max-w-xs">
+      <form action={uploadPicture} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <label htmlFor="file" className="block text-gray-700 text-sm font-bold mb-2">
+          JPG
+        </label>
+        <input
+          name="file"
+          type="file"
+          accept="image/png, image/jpeg, image/jpg"
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        />
+
+        <br />
+        <label htmlFor="species" className="block text-gray-700 text-sm font-bold mb-2">
+          Species
+        </label>
+        <input
+          name="species"
+          type="text"
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        />
+
+        <br />
+        <label htmlFor="number" className="block text-gray-700 text-sm font-bold mb-2">
+          Number
+        </label>
+        <input
+          name="number"
+          type="text"
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        />
+
+        <br />
+        <input
+          type="submit"
+          className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+          value="Submit"
+        />
+      </form>
     </main>
   );
 }
