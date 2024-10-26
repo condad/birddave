bad4890c920c2c39433c9d1e509f5b0bde36d92e
Connor Sullivan <connor.sullivan@kraken.tech>
2024-10-26 00:58:24 -0400

    Add logout link

--- app/AuthStatus.tsx
+++ app/AuthStatus.tsx
@@ -1,9 +1,35 @@
 "use client";
 
-import { useState } from "react";
 import { type User } from "./types";
-import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export function AuthStatus({ currentUser }: { currentUser: User | null }) {
-  return <div className="text-sm">{currentUser === null ? "Not logged in" : "Logged in as " + currentUser.email}</div>;
+  const router = useRouter();
+
+  function logout() {
+    console.log("Logging out");
+    document.cookie = "idToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    router.push("/");
+    return router.refresh();
+  }
+
+  if (currentUser === null) {
+    return (
+      <div className="text-sm">
+        <a href="/upload" className="text-blue-500 hover:underline hover:inline">
+          Login
+        </a>
+      </div>
+    );
+  }
+
+  return (
+    <div className="text-sm">
+      {"Logged in as " + currentUser.email}
+      {" | "}
+      <a href="#" onClick={logout} className="text-blue-500 hover:underline">
+        Logout
+      </a>
+    </div>
+  );
 }
