19e6b81e659357908f549bf0ed9b004f644e185e
Connor Sullivan <connor.sullivan@kraken.tech>
2025-04-20 19:08:24 -0400

    Upgrade aws-sdk libs

--- sst.config.ts
+++ sst.config.ts
@@ -65,7 +65,7 @@ export function Infra({ stack }) {
 
   const auth = new Cognito(stack, "auth", {
     cdk: {
-      userPool: pool,
+      userPool: pool as any, // Due to incompatibility between SST and CDK types
       userPoolClient: client,
     },
   });
